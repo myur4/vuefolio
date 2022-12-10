3e0d001fc2bd70e40d214d4a15e4de5ff260acbd
George Santos <47012804+myur4@users.noreply.github.com>
2022-12-10 15:23:58 -0300

    'About' section's style finished

--- components/About.vue
+++ components/About.vue
@@ -1,5 +1,35 @@
 <template>
     <section class="wrapper">
-        <h1>Oi</h1>
+        <div class="flex flex-col gap-8">
+            <h1 class="mb-3 underline decoration-2 decoration-secondary text-4xl font-extrabold text-primary text-center md:text-left">About Me</h1>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl">
+            <div class="md:col-span-2">
+                <p class="font-light text-2xl text-center md:text-left text-primary">
+                    hello! i'm an computer engineering (2/10) undergrad at <strong>UNIRUY,</strong>
+                    my interests are in chatbot development, <strong>backend deployment and
+                    microservices</strong>
+                    <br/><br/>
+                    currently I'm looking for a job or internship as a junior developer
+                    especially focusing into working with C#, Node.JS and derivated frameworks
+                    from those ones.
+                </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center md:text-left gap-2">
+                <div class="text-center">
+                    <span class="text-primary font-bold">FULL NAME</span>
+                    <p class="text-accent font-bold text-center">GEORGE SANTOS DE JESUS SANTIAGO</p>
+                </div>
+                <div class="text-center">
+                    <span class="text-primary font-bold">DATE OF BIRTH</span>
+                    <p class="text-accent font-bold text-center">03 September 2001</p>
+                </div>
+                <div class="text-center">
+                    <span class="text-primary font-bold">EMAIL</span>
+                    <p class="text-accent font-bold text-center">hello@myura.dev</p>
+                </div>
+            </div>
+        </div>
+        </div>
     </section>
 </template>
