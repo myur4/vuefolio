033932dce82d4da0c328c1fa21be17255c9b172d
George Santos <47012804+myur4@users.noreply.github.com>
2022-12-16 18:25:29 -0300

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
     <nav class="flex justify-between items-center w-full p-4 lg:p-8 h-20 bg-base-100 fixed">
-        <a class="text-4xl font-extrabold text-primary hover:underline" href="#home">George Santos</a>
+        <a class="text-4xl font-extrabold text-primary hover:underline" href="#home">George S.</a>
 
         <div>
             <BurguerMenu v-if="!showBurguer" @click="altMenu()" class="lg:hidden" />
             <div v-if="showBurguer"
-                class="flex mt-20 z-10 flex-col border-solid w-32 border-secondary rounded-lg border-2 p-4 items-center gap-2 bg-base-100">
+                class="flex mt-20 flex-col border-solid w-32 border-secondary rounded-lg border-2 p-2 items-center gap-2 bg-base-100">
                 <BurguerMenu @click="altMenu()" class="relative left-10 mb-2 fill-primary" />
                 <a class="link-primary hover:underline hover:text-secondary cursor-pointer" href="#about">About Me</a>
                 <a class="link-primary hover:underline hover:text-secondary cursor-pointer" href="#skills">Skills</a>
